39944643f86ca17024f0f8f7d3ea23c20f42b294
deonleer <deon.leer96@gmail.com>
2021-03-10 00:26:59 +0000

    commit before large refactor

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
+
 import About_Me from './About_Me';
 import Experience from './Experience';
 import Freelance_Services from './Freelance_Services';
 import Projects from './Projects';
 import Resume from './Resume';
 import Social_Medias from './Social_Medias';
+
 
 export default function Page() {
 
