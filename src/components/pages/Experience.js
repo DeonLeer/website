d791dd8300127dc41152baa235fa0990f7a6a683
deonleer <deon.leer96@gmail.com>
2021-03-10 00:04:44 +0000

    created pages

---
+++ src/components/pages/Experience.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default function Experience() {
+  return (
+    <div className="Experience">
+      <h1>Experience</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin gravida scelerisque tincidunt. Nullam congue odio elit, ut varius dui gravida vel. Maecenas sagittis tincidunt ligula nec lacinia. Ut ac porttitor leo, et vestibulum massa. Nam sit amet sodales nisi, ut ultrices augue. Mauris tempor tellus interdum neque vulputate luctus. Quisque hendrerit pretium tortor, non sollicitudin elit tempus ac. Morbi diam erat, imperdiet id felis eget, tincidunt faucibus tellus. Etiam hendrerit gravida pulvinar. Aliquam vitae bibendum erat, sit amet mattis tellus. Vivamus sed urna rhoncus, semper orci tempus, volutpat lectus. Phasellus varius dapibus pharetra. Aliquam erat volutpat. Morbi a dolor congue, luctus quam eu, vehicula lorem. Mauris malesuada diam in magna molestie, quis blandit magna pharetra.</p>
+
+      <p>Nullam quis nibh a metus tincidunt posuere. Nulla nibh enim, hendrerit porttitor volutpat mollis, luctus eu urna. Pellentesque dui lectus, scelerisque non maximus at, hendrerit id diam. Morbi suscipit, tellus eget interdum sodales, lorem ante commodo ante, at luctus ipsum libero id ex. Quisque suscipit felis at arcu elementum aliquet. Sed pulvinar nibh orci, a sodales velit tristique sit amet. Aenean dignissim nunc eu tempor pulvinar. Vestibulum ut tellus ullamcorper, tincidunt velit vel, gravida lectus. Fusce vitae dui interdum purus tincidunt ultrices eu tincidunt mi. Duis dictum metus et velit porta sollicitudin. Vestibulum lobortis purus nec purus tempor, sit amet elementum erat feugiat. Sed accumsan dui sem, vitae dapibus orci placerat eget. Aliquam ultrices nisi nulla, consequat varius tortor lobortis et.</p>
+    </div>
+  )
+}
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,5 +1,21 @@
+import React from 'react';
+import About_Me from './About_Me';
+import Experience from './Experience';
+import Freelance_Services from './Freelance_Services';
+import Projects from './Projects';
+import Resume from './Resume';
+import Social_Medias from './Social_Medias';
+
 export default function Page() {
+
   return (
-    <h1>Current Page</h1>
+    <div className="Page">
+      <About_Me />
+      <Projects />
+      <Resume />
+      <Experience />
+      <Freelance_Services />
+      <Social_Medias />
+    </div>
   )
 }
