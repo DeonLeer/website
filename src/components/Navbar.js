b0cd8cb25a4b685a6c2737a451efa6d24f8164ba
deonleer <deon.leer96@gmail.com>
2021-03-11 00:00:21 +0000

    more styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React from "react";
+import Button from '@material-ui/core/Button';
 
 export default function Navbar(props) {
+  const tdStyle = props.notMode === 'Light' ? {backgroundColor: 'grey'} : {backgroundColor: 'white'};
+  const buttonStyle = props.notMode === 'Light' ? {color: 'white', width: '100%'} : { width: '100%' };
 
   return (
     <div className="Navbar" id="Navbar">
@@ -11,14 +14,14 @@ export default function Navbar(props) {
       <div className="NavLinks">
         <table>
           <tr>
-            <td onClick={props.aboutMe}>About me</td>
-            <td onClick={props.projects}>Projects</td>
-            <td onClick={props.resume}>Resume</td>
+            <td style={tdStyle}><Button onClick={props.aboutMe} style={buttonStyle}>About me</Button></td>
+            <td style={tdStyle}><Button onClick={props.projects} style={buttonStyle}>Projects</Button></td>
+            <td style={tdStyle}><Button onClick={props.resume} style={buttonStyle}>Resume</Button></td>
           </tr>
           <tr>
-            <td onClick={props.projects}>Projects</td>
-            <td onClick={props.freelanceServices}>Freelance Services</td>
-            <td onClick={props.contactMe}>Contact me</td>
+            <td style={tdStyle}><Button onClick={props.projects} style={buttonStyle}>Projects</Button></td>
+            <td style={tdStyle}><Button onClick={props.freelanceServices} style={buttonStyle}>Freelance Services</Button></td>
+            <td style={tdStyle}><Button onClick={props.contactMe} style={buttonStyle}>Contact me</Button></td>
           </tr>
         </table>
       </div>
