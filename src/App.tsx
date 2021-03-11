b8eb106c983c47fea6415f5b5ce8c7ca8089cd5b
talor.hammond <talor.hammond@kiwiwealth.co.nz>
2021-03-11 19:18:00 +1300

    convert App.js -> typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import React, { useState, useRef } from "react"
+import React, { useState, useRef, RefObject } from "react"
 import { ThemeProvider } from "styled-components"
 import { GlobalStyle } from "./components/GlobalStyle"
 import { lightTheme, darkTheme } from "./components/Themes"
@@ -13,7 +13,6 @@ import Resume from "./components/pages/Resume"
 import ContactMe from "./components/pages/ContactMe"
 import Bio from "./components/pages/Bio"
 
-// components
 // styling-techniques: pick one pattern -> regular .css, scss <- use scss
 
 export default function App() {
@@ -25,22 +24,23 @@ export default function App() {
     notTheme === "Dark" ? setNotTheme("Light") : setNotTheme("Dark")
   }
 
-  // Create a react-hook for useScrollTo or something like that.
-  // Re-usability, less repeated code
-  const aboutMe = useRef(null)
-  const experience = useRef(null)
-  const freelanceServices = useRef(null)
-  const projects = useRef(null)
-  const resume = useRef(null)
-  const contactMe = useRef(null)
-  const bio = useRef(null)
+  const aboutMe = useRef<HTMLDivElement>(null)
+  const experience = useRef<HTMLDivElement>(null)
+  const freelanceServices = useRef<HTMLDivElement>(null)
+  const projects = useRef<HTMLDivElement>(null)
+  const resume = useRef<HTMLDivElement>(null)
+  const contactMe = useRef<HTMLDivElement>(null)
+  const bio = useRef<HTMLDivElement>(null)
 
-  const scroll = (reference) => {
-    let windowHeight = window.innerHeight
+  const scroll = (reference: RefObject<HTMLDivElement>) => {
+    const windowHeight = window.innerHeight
 
     window.scrollTo({
       behavior: "smooth",
-      top: reference.current.offsetTop - windowHeight * 0.17,
+      // Checking reference.current isn't falsy before grabbing .offsetTop
+      top: reference.current
+        ? reference.current.offsetTop - windowHeight * 0.17
+        : undefined,
     })
   }
 
@@ -72,25 +72,35 @@ export default function App() {
           />
         </div>
         <div ref={aboutMe}>
-          <AboutMe viewMore={() => scroll(bio)} />
+          <AboutMe onClickViewMore={() => scroll(bio)} />
         </div>
         <div ref={bio}>
           <Bio viewMore={() => scroll(projects)} />
         </div>
         <div ref={projects}>
-          <Projects viewMore={() => scroll(resume)} />
+          <Projects
+          // viewMore={() => scroll(resume)}
+          />
         </div>
         <div ref={resume}>
-          <Resume viewMore={() => scroll(experience)} />
+          <Resume
+          // viewMore={() => scroll(experience)}
+          />
         </div>
         <div ref={experience}>
-          <Experience viewMore={() => scroll(freelanceServices)} />
+          <Experience
+          // viewMore={() => scroll(freelanceServices)}
+          />
         </div>
         <div ref={freelanceServices}>
-          <FreelanceServices viewMore={() => scroll(contactMe)} />
+          <FreelanceServices
+          // viewMore={() => scroll(contactMe)}
+          />
         </div>
         <div ref={contactMe}>
-          <ContactMe viewTop={() => scroll(aboutMe)} />
+          <ContactMe
+          // viewTop={() => scroll(aboutMe)}
+          />
         </div>
       </div>
     </ThemeProvider>
